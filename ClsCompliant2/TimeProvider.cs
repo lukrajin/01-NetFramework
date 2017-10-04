764232738a3fba8ead83f60fb17fc1cc54a6875f
Julijan Šribar <julijan.sribar@gmail.com>
2017-10-04 23:26:07 +0200

    Add project files.

---
+++ ClsCompliant2/TimeProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+[assembly:CLSCompliant(true)]
+namespace ProvideTime
+{
+    public class TimeProvider
+    {
+        public static string GetDateTime(string ime)
+        {
+            return DateTime.Now.ToShortTimeString();
+        }
+
+        public static string GetDateTime()
+        {
+            return DateTime.Now.ToString(); 
+        }
+
+        // ako promijenimo private u public, prevoditelj će javiti upozorenje
+        private static string GetDatetime()
+        {
+            return DateTime.UtcNow.ToLongDateString();
+        }
+    }
+}
---
+++ VbNetConsole/Module1.vb
@@ -0,0 +1,12 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim sum As Integer = 0
+        For i = 1 To 100 Step 1
+            sum = sum + i
+        Next
+        Console.WriteLine(sum)
+    End Sub
+
+End Module
